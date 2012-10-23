d3ca2cb3ee46b92d1962412b16d4ff987253d4f3
Alexander Blakov <thexobbitt@gmail.com>
2012-10-23 21:09:52 +0300

    Added unique option for Node.name

--- cc/models.py
+++ cc/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Node(models.Model):
-    name = models.CharField(max_length = 20, verbose_name = u'Node number', help_text = u'Example: Node_N, where N = number')
+    name = models.CharField(max_length = 20, unique = 'True', verbose_name = u'Node number', help_text = u'Example: Node_N, where N = number')
     ip = models.IPAddressField(verbose_name = u'IP address', help_text = u'Example: 10.0.0.1')
     mem_max = models.IntegerField(verbose_name = u'Memory maximum', help_text = u'In MB. Example: 1024')
     cpu_num = models.IntegerField(verbose_name = u'Number of CPU', help_text = u'Number of cores or processors. Example: 2')
